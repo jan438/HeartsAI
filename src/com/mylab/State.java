a008a6882b6bb94a40a205a7d78d919490b280b0
Jan Boon <janboon438@gmail.com>
2016-07-13 09:35:02 +0200

    Countcardsplayed added

--- src/com/mylab/State.java
+++ src/com/mylab/State.java
@@ -35,7 +35,8 @@ class State {
 	}
 
 	boolean isGameValid() {
-		return cardsPlayed.size() < 52;
+		int countcardsplayed = cardsPlayed.size();
+		return countcardsplayed < 52;
 	}
 
 	boolean validRound() {
